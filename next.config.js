27db9a8de66478edc9e21294128bb2afdad14138
Carlos Paredes <00028516@uca.edu.sv>
2024-02-15 08:25:19 -0600

    [UPDATE] - Next js config

--- next.config.js
+++ next.config.js
@@ -1,40 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     trailingSlash: true,
     reactStrictMode: true,
-    output: 'export',
     compiler: {
-      styledComponents: true,
+        styledComponents: true,
     },
-    async redirects() {
-      return [
-        {
-          source: "/pt-BR/",
-          destination: "/",
-          permanent: true,
-        },
-      ];
-    },
-    async headers() {
-      return [
-        {
-          source: "/api/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            {
-              key: "Access-Control-Allow-Methods",
-              value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-            },
-            {
-              key: "Access-Control-Allow-Headers",
-              value:
-                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
+};
+
+module.exports = nextConfig;
